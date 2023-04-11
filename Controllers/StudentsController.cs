cbd2b6bd51e662e1efc870a6a68921a405cfec62
Ali <ali.cruz@me.com>
2023-04-10 22:22:21 -0600

    Carga inicial proyecto

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+        
+
+
+        // GET: Students
+        public async Task<IActionResult> Index(
+            string sortOrder,
+            string currentFilter,
+            string searchString,
+            int? pageNumber)
+        {
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            } else
+            {
+                searchString = currentFilter;
+            }
+                        
+            ViewData["CurrentFilter"] = searchString;
+
+            // LINQ - IQueryable
+            var studentsEntity = from st in _context.Students
+                                 select st;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentsEntity = studentsEntity.Where(
+                    st => st.LastName.Contains(searchString) ||
+                    st.FirstMidName.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsEntity = studentsEntity.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsEntity = studentsEntity.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsEntity = studentsEntity.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsEntity = studentsEntity.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            int pageSize = 3;
+
+            //return View(await _context.Students.ToListAsync());
+            //return View(await studentsEntity.AsNoTracking().ToListAsync());
+            return View(await PaginatedList<Student>.CreateAsync(
+                studentsEntity.AsNoTracking(), 
+                pageNumber ?? 1, 
+                pageSize));
+        }
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .Include(s => s.Enrollments)
+                .ThenInclude(e => e.Course)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(
+            [Bind("LastName,FirstName,EnrollmentDate")] Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            } catch (DbUpdateException /* e */)
+            {
+                ModelState.AddModelError("", "Unable to sabe changes." +
+                    "try again");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var studentToUpdate = await _context.Students.FirstOrDefaultAsync(student => student.ID == id);
+
+            if (await TryUpdateModelAsync<Student> (
+                studentToUpdate,
+                "",
+                s => s.FirstMidName, 
+                s => s.LastName, 
+                s => s.EnrollmentDate))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                } catch (DbUpdateException e)
+                {
+                    ModelState.AddModelError("", "Unable to save changes." +
+                        "Try again. Error: " + e);
+                }
+            }
+
+            return View(studentToUpdate);
+        }
+
+        // GET: Students/Delete/5
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] = "Delete failed";
+            }
+
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            try           
+            {
+                _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            } catch (DbUpdateException /* e */)
+            {
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+            }            
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    // Entidad student, en el contexto de base de datos.
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required]        
+        [StringLength(50, MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+        // Si una propiedad de navegación puede contener varias entidades
+        // entonces resulta en un listado y debe usar: 
+        // ICollection, List o HashSet
+        // Una propiedad de navegación contiene todas las entidades de relación
+        // 
+        public ICollection<Enrollment> Enrollments { get; set; }
+        // ICollection crea un HashSet<T> de forma predeterminada
+
+    }
+}
